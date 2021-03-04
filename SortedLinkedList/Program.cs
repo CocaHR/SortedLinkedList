621ad6db3a24089936483c113a62f1f6d5d1fec0
CocaHR <oleksandr.chuchko@nure.ua>
2021-03-04 13:37:53 +0200

    Add project files.

---
+++ SortedLinkedList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SortedLinkedList
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node()
+        {
+            next = null;
+        }
+        public Node(int item, Node link=null)
+        {
+            data = item;
+            next = link;
+        }
+    }
+    class List
+    {
+        int count;
+        Node first;
+        public Node head;
+        public List()
+        {
+            count = 0;
+            first = null;
+        }
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
